0befcf98a7a243a158a55fb0446faa1426234e2b
Go Nakano <g.nakano555@gmail.com>
2020-07-24 17:50:43 +0900

    initial commit

---
+++ src/component/Layout.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Layout = () => <div className="layout-container">TEST</div>;
+
+export default Layout;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+// const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 8080,
+  },
+  // plugins: [new htmlWebpackPlugin({ template: './index.html' })],
+};
