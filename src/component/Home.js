105d85e64161815eb1fc564e1d8356528c4d188f
Go Nakano <g.nakano555@gmail.com>
2020-07-26 03:21:12 +0900

    succeess to read background image

---
+++ src/component/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div className="home-container">test</div>;
+};
+
+export default Home;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Layout from './component/Layout';
 import './assets/styles/application.scss';
 const rootContainer = document.getElementById('rootContainer');
-console.log(document.getElementById('rootContainer'));
+
 const App = () => <Layout />;
 
 ReactDOM.render(<App />, rootContainer);
