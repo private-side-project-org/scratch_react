68da71c44be5156d54dddd10dcdb3ac5acd64673
Go Nakano <g.nakano555@gmail.com>
2020-07-27 00:16:17 +0900

    implemented move page

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import CONSTANTS from '../utils/constants';
 import { Row, Col } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const Navbar = () => {
   const { NAVITEMS } = CONSTANTS;
   const listItem = () => {
-    const items = NAVITEMS.map((navItem, key) => (
-      <li className="navbar-items">{navItem}</li>
-    ));
+    const history = useHistory();
+    const items = NAVITEMS.map((navItem, index) => {
+      const handleClick = () => history.push(`/${navItem.toLowerCase()}`);
+      return (
+        <li
+          key={navItem + index}
+          className="navbar-items"
+          onClick={handleClick}
+        >
+          {navItem}
+        </li>
+      );
+    });
     return (
       <Row className="navbar-container">
         <Col xs={12} sm={8} md={8} lg={6} className="navbar-listcontainer">
@@ -16,7 +27,6 @@ const Navbar = () => {
       </Row>
     );
   };
-
   return listItem();
 };
 
