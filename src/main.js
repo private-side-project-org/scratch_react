85b4a5bdf67715ca0e30934552ba5058def2f273
Go Nakano <g.nakano555@gmail.com>
2020-07-26 23:12:42 +0900

    added bootstrap library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './component/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/application.scss';
 const rootContainer = document.getElementById('rootContainer');
 
