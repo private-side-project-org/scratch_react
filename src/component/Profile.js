a296bbba248719cc4eabc3b8705e05c357ec766f
Go Nakano <g.nakano555@gmail.com>
2020-07-28 20:57:44 +0900

    created profile-page add tags

---
+++ src/component/Profile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import facebookIcon from '../assets/facebook.png';
+import instagramIcon from '../assets/instagram.png';
+import twitterIcon from '../assets/twitter.png';
+
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <span>Profile</span>
+      <Row>
+        <Col>
+          <ul>
+            <li>Name</li>
+            <li>Job</li>
+            <li>Email</li>
+          </ul>
+        </Col>
+        <Col>
+          <ul>
+            <li>Go Nakano</li>
+            <li>Software Engineer</li>
+            <li>g.nakano555@gmail.com</li>
+          </ul>
+        </Col>
+      </Row>
+      <div>
+        <Image src={facebookIcon} alt="facebookIcon" />
+        <Image src={instagramIcon} alt="instagramIcon" />
+        <Image src={twitterIcon} alt="twitterIcon" />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
