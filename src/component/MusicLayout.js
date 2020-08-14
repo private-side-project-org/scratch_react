c664d9616873cfc4d21dc3ce34ffc3cb0b818425
Go Nakano <g.nakano555@gmail.com>
2020-08-15 01:25:25 +0900

    replaced local video to vimeo

--- src/component/MusicLayout.js
+++ src/component/MusicLayout.js
@@ -6,6 +6,30 @@ const MusicLayout = ({ component }) => {
   return (
     <div className="layout-container">
       <Navbar />
+      <ReactPlayer
+        url="https://vimeo.com/447842582"
+        playing
+        loop
+        height="100vh"
+        width="100vw"
+        style={{
+          margin: 0,
+          position: 'absolute',
+          opacity: 0.8,
+          overFlow: 'hidden',
+        }}
+        muted
+        config={{
+          vimeo: {
+            playerOptions: {
+              background: 1,
+              controls: 0,
+              muted: true,
+              maxwidth: '10vw',
+            },
+          },
+        }}
+      />
       {component}
     </div>
   );
