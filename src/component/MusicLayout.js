f8910d27f90c00e863ad1b53b40d98befb3f3b74
Go Nakano <g.nakano555@gmail.com>
2020-08-15 17:49:09 +0900

    fixed position of navbar

--- src/component/MusicLayout.js
+++ src/component/MusicLayout.js
@@ -5,14 +5,7 @@ const MusicLayout = ({ component }) => {
   return (
     <div className="embed-container">
       <Navbar />
-      <iframe
-        src="https://player.vimeo.com/video/448045945?"
-        width="640"
-        height="360"
-        frameborder="0"
-        allow="autoplay; fullscreen"
-        allowfullscreen
-      ></iframe>
+      <iframe src="https://player.vimeo.com/video/448045945?autoplay=1&loop=1&background=1"></iframe>
       {component}
     </div>
   );
