75273f0dde80675aea8969c63eacb6be825ff809
Go Nakano <g.nakano555@gmail.com>
2020-08-24 01:48:00 +0900

    added animation path in photos

--- src/component/MusicLayout.js
+++ src/component/MusicLayout.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 
 const MusicLayout = ({ component }) => {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="embed-container">
-      <Navbar />
-      <iframe src="https://player.vimeo.com/video/448045945?autoplay=1&loop=1&background=1"></iframe>
-      {component}
+      <iframe
+        src="https://player.vimeo.com/video/448045945?autoplay=1&loop=1&background=1"
+        onLoad={() => setLoading(false)}
+      ></iframe>
+      {loading ? (
+        <span classname="load-image-container">LOADING</span>
+      ) : (
+        <>
+          <Navbar />
+          {component}
+        </>
+      )}
     </div>
   );
 };
--- src/component/Router.js
+++ src/component/Router.js
@@ -6,21 +6,23 @@ import Music from '../component/Music';
 import Profile from '../component/Profile';
 import Scene1 from './canvas/components/scene1';
 import MusicLayout from './MusicLayout';
+import Photo from './Photo';
 
 const MainRoute = () => {
   const homeComponent = () => <Layout component={<Home />} />;
   const musicComponent = () => <MusicLayout component={<Music />} />;
   const profileComponent = () => <Layout component={<Profile />} />;
   const canvasCompA = () => <Layout component={<Scene1 />} />;
+  const photoComponent = () => <Layout component={<Photo />} />;
   return (
     <Switch>
       <Redirect exact from="/" to="/home" />
-      <Redirect exact from="/photos" to="/home" />
       <Redirect exact from="/blog" to="/home" />
       <Route path="/home" component={homeComponent} />
       <Route path="/music" component={musicComponent} />
       <Route path="/profile" component={profileComponent} />
       <Route path="/canvasA" component={canvasCompA} />
+      <Route path="/photos" component={photoComponent} />
     </Switch>
   );
 };
