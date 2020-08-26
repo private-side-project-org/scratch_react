f19864d27d8f4515f024885d5f3bc446ad25eca0
Go Nakano <g.nakano555@gmail.com>
2020-08-27 00:29:01 +0900

    set another photo

--- src/component/Photo.js
+++ src/component/Photo.js
@@ -1,11 +1,12 @@
-import React, { useState, Component } from 'react';
-import Navbar from './Navbar';
+import React from 'react';
 import PhotoOne from './animationPhoto/PhotoOne';
+import PhotoTwo from './animationPhoto/PhotoTwo';
 
 const Photo = () => {
   return (
     <div className="photo-container">
       <PhotoOne />
+      <PhotoTwo />
     </div>
   );
 };
